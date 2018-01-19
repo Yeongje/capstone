beded21d3696717d716a851197a25b3829108034
DESKTOP-CPEQ2TJ\mykoj <mykoji12@naver.com>
2018-01-19 13:10:32 +0900

    Create Login/Singup/Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,10 @@ from django.conf import settings
 
 
 urlpatterns = [
-
+    url(r'^signup/$',views.signup, name ='signup'),
+    url(r'^login/$', auth_views.login, name='login', kwargs={
+    #    'authentication_form': LoginForm,
+        'template_name': 'accounts/login_form.html', }),
+    url(r'^logout/$', auth_views.logout, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
+    url(r'^profile/$',views.profile, name ='profile'),
 ]
